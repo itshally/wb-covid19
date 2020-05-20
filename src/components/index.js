0f0354669bbff7b349b7359e55f2cee5cbef1943
HB <hbianan@gmail.com>
2020-05-20 18:59:40 -0400

    files added
    - added missing files

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/**
+ *  This file exports all of the components from its respective folder
+ *  which is also used in line 3 of App.js file
+*/
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';

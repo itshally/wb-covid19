7c6d9c79f9bb2a551d6e499a7a02a486412e943c
HB <hbianan@gmail.com>
2020-05-24 17:43:55 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@
 export { default as Cards } from './Cards/Cards';
 export { default as Chart } from './Chart/Chart';
 export { default as CountryPicker } from './CountryPicker/CountryPicker';
+export { default as ExclusiveContent } from './ExclusiveContent/ExclusiveContent';

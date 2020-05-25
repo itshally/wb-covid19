3b368ee69ebe22005b4e6e5436dfd739b13cf8ae
HB <hbianan@gmail.com>
2020-05-25 00:33:22 -0400

    Update ExclusiveContent.jsx

--- src/components/ExclusiveContent/ExclusiveContent.jsx
+++ src/components/ExclusiveContent/ExclusiveContent.jsx
@@ -1,14 +1,75 @@
-import React from "react";
+import React, {Component} from "react";
 import { Container } from "../../../node_modules/@material-ui/core";
 import './ExclusiveContent.module.css'
 
-const ExclusiveContent = () =>{
-    
-    return(
-        <Container className="hidden" id="exclusive">
-            <p>this is an exclusive content</p>
-        </Container>
-    );
-};
+export default class ExclusiveContent extends Component {
+    constructor(props){
+        super(props);
 
-export default ExclusiveContent;
+        this.state = {
+            data: [],
+            news: []
+        }
+    }
+
+    componentDidMount(){
+        const settings = {
+            "url": "https://www.who.int/rss-feeds/news-english.xml",
+            "method": "GET",
+            "timeout": 0,
+          };
+        const RSS_URL = settings.url;
+
+        fetch(RSS_URL)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+
+            this.setState({data: 
+                {
+                    title: data.querySelectorAll('title'),
+                    link: data.querySelectorAll('link')
+                }
+            })
+
+            
+            // THIS DISPLAYS THE TITLE ON THE CONSOLE
+            console.log(this.state.data.title[0].innerHTML);
+
+            this.state.data.title.forEach( x => {
+
+                // console.log(x.innerHTML)
+                this.state.news.push(
+                    x.innerHTML
+                )
+
+            })
+
+            
+        });
+
+    }
+
+    render(){
+        const {title, link} = this.state.data;
+        
+        console.log(this.state.news)
+
+        const { news } = this.state;
+        console.log(news)
+
+
+        return(
+            
+            <div>
+                
+                <ul>
+                    {
+                        // news.map(x => <li key={x}>{x}</li>)
+                    }
+                </ul>
+                
+            </div>
+        );
+    }
+}

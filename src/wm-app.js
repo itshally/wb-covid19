f642f20f01c2d4576d488358f9640f661306ec2c
HB <hbianan@gmail.com>
2020-06-04 11:26:34 -0400

    testing
    - issue on deployment
    - news article are not showing when the monetization started

--- src/wm-app.js
+++ src/wm-app.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
     stopButton.disabled = true
     startButton.disabled = false
 
-    document.getElementById('exclusive').classList.add('App_hidden__3gJL7')
+    document.getElementById('exclusive').classList.add('hidden')
   })
 
   startButton.addEventListener('click', () => {
@@ -37,7 +37,7 @@ window.addEventListener('load', () => {
     stopButton.disabled = false
     startButton.disabled = true
 
-    document.getElementById('exclusive').classList.remove('App_hidden__3gJL7')
+    document.getElementById('exclusive').classList.remove('hidden')
 
   })
 })

d6f76924417b19cf9397cb8ea31785637c918ec7
HB <hbianan@gmail.com>
2020-06-03 21:50:34 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Cards, Chart, CountryPicker, ExclusiveContent, Footer } from "./compone
 import { fetchData } from "./api";
 import Buttons from './components/webmonetize/buttonmonetize'
 import logo from './images/logo.jpg';
+// import './wm-app'
 
 class App extends React.Component {
   state = {
@@ -24,9 +25,12 @@ class App extends React.Component {
     this.setState({ data: fetchedData, country: country });
   }
 
+
+  
+
   render() {
     const { data, country } = this.state;
-
+    
     return (
       <div className={styles.container}>
         <img src={logo} alt="logo" className={styles.logo}/>
@@ -34,11 +38,33 @@ class App extends React.Component {
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart data={data} country={country}/> 
         <Buttons />
+        
+        <pre id="container">
+        </pre>
+        
         <ExclusiveContent />
+
+        {/* !-- container to display monetization events --> */}
+        <pre id="container">
+        </pre>
+
+        {/* <!-- error messages --> */}
+        <div id="error-no-monetization" className={styles.error}>
+          Note: In order to see any events here, you need to have an extension installed from a web monetization provider, like <a href="https://coil.com">coil.com</a>.
+          (<a href="https://chrome.google.com/webstore/detail/coil/locbifcbeldmnphbgkdigjmkbfkhbnca">chrome</a> <a href="https://addons.mozilla.org/en-US/firefox/addon/coil/">firefox</a>)
+        </div>
+        <div id="error-wrong-protocol" className={styles.error}>
+          Error: This demo must be <a href="https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server">run from a server</a>.
+        </div>
+        <div id="error-timeout" className={styles.error}>
+          Warning: No monetization events occurred after six seconds. This probably indicates that you have a web monetization extension, but no active account. Get one at <a href="https://coil.com">coil.com</a>.
+        </div>
+
         <Footer />
       </div>
     );
   }
+  
 }
 
 export default App;

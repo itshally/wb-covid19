c8dfe89a61a03bdb5383516cfacdbe2b2260adff
HB <hbianan@gmail.com>
2020-06-08 22:15:23 -0400

    Update wm-app.js

--- src/wm-app.js
+++ src/wm-app.js
@@ -1,5 +1,16 @@
 function showMonetizationState() {
   document.getElementById('state').innerText = document.monetization.state
+
+  if(document.monetization.state === "pending"){
+    console.log('still pending...')
+  }else 
+    if(document.monetization.state === "stopped"){
+      console.log('monetization is stopped')
+      document.getElementById('exclusive').classList.add('hidden');
+  }else{
+    console.log('monetization is started')
+    document.getElementById('exclusive').classList.remove('hidden');
+  }
 }
 
 if (document.monetization) {
@@ -12,8 +23,6 @@ window.addEventListener('load', () => {
   if (!document.monetization) {
     document.getElementById('state').innerText = 'Not enabled in browser';
     document.getElementById('exclusive').classList.add('hidden');
-    document.getElementById('stop-button').setAttribute('disabled', true);
-    document.getElementById('start-button').setAttribute('disabled', true);
   } else {
     showMonetizationState()
   }
@@ -22,24 +31,19 @@ window.addEventListener('load', () => {
   const startButton = document.getElementById('start-button')
   const monetizationTag = document.querySelector('meta[name="monetization"]')
 
-  //removes the monetization tag when the app starts loading
-  monetizationTag.remove();
-
+  // stop button
   stopButton.addEventListener('click', () => {
     monetizationTag.remove()
 
     stopButton.disabled = true
     startButton.disabled = false
-
-    document.getElementById('exclusive').classList.add('hidden');
   })
 
+  // start button
   startButton.addEventListener('click', () => {
     document.head.appendChild(monetizationTag)
 
     stopButton.disabled = false
     startButton.disabled = true
-
-    document.getElementById('exclusive').classList.remove('hidden');
   })
 })
